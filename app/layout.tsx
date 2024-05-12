7bad886306090b0875ac593c1929599308bb4b11
Anna Clara Rodrigues <annarms09@gmail.com>
2024-05-12 20:04:02 -0300

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" style={{ height: '100%' }}>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/TabIcon.svg" type="image/jpeg" />
+      </head>
       <body style={{ margin: '0px' }}>{children}</body>
     </html>
   )
